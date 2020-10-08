d7bf6c8901c5faa4363fa1fe7c6faea213f3cde8
Randy Flores Garcia <rfull@MacBook-Pro-de-Randy.local>
2020-10-08 01:59:44 -0500

    Initial commit

---
+++ app/src/main/java/mx/com/randyfull8/prob/MainActivity.java
@@ -0,0 +1,17 @@
+package mx.com.randyfull8.prob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("Hola mundo");
+    }
+
+}
